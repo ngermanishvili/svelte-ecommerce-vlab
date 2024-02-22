266a9a52aee40b6aed1498020e1a6d55ba35ac68
Nika Germanishvili <114104887+ngermanishvili@users.noreply.github.com>
2024-02-22 15:53:03 +0400

    Update +server.ts
    HIDE TEST ST ID

--- src/routes/api/stripeCheckout/+server.ts
+++ src/routes/api/stripeCheckout/+server.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from "./$types";
 import Stripe from 'stripe';
 
-const SECRET_STRIPE_KEY = "sk_test_51LfAvvGEQ8u28rQ5IvVpV2iXwMEIRvDvMNLTyzfM6eXlxQZpLlyrXFjiRP0Nj64t7EDcXm3tnDxnEvShrmCwUDgW00TaZN1cO8";
+const SECRET_STRIPE_KEY = "SECRET";
 const stripe = new Stripe(SECRET_STRIPE_KEY, {
     apiVersion: "2022-11-15"
 });
